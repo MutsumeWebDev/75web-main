e2881d2857d609817e10bd451f0a44a3fc5b9cdb
hamanishi932 <136817555+hamanishi932@users.noreply.github.com>
2024-09-07 20:13:10 +0900

    スライダー機能 Fixes #2

--- system.js
+++ system.js
@@ -1,17 +1,32 @@
-function mutsume_header() {
-    document.write(
+const slider = document.querySelector('#slider');
 
-    )
-}
-//slider
-const dataset[
+const dataset = [
     {
-
-    }
-]
-function mutsume_slider() {
-
+        title: 'テーマ',
+        img: 'character_mutsumechan.png',
+        url: '',
+    },
+    {
+        title: '乃紫さんライブ',
+        img: 'character_mutsumechan.png',
+        url: '',
+    },
+    {
+        title: '梶裕貴さんトークショー',
+        img: 'character_mutsumechan.png',
+        url: '',
+    },
+    {
+        title: 'ホームカミングデー',
+        img: 'character_mutsumechan.png',
+        url: '',
+    },
+];
+for (let i = 0; i < dataset.length; i++){
+    const {title, img, url} = dataset[i];
+    const content = `<div class="slider${i}"><a href="${url}"><img src="${img}" alt="${title}"><h2>${title}</h2></a></div>`;
+    slider.insertAdjacentHTML('beforeend', content);
 }
-function mutsume_footer() {
 
-}
+//header & menubar
+const header = document.querySelector('#header');
