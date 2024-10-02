04b431efbe1bf4e004fe7338c76e5197f122c83c
hamanishi932 <136817555+hamanishi932@users.noreply.github.com>
2024-10-02 16:49:58 +0900

    スライダーのデザイン Fixes #23

--- slider.js
+++ slider.js
@@ -1,9 +1,28 @@
-$('#slider').slick({
-  autoplay: true,//自動的に動き出すか。初期値はfalse。
-  infinite: true,//スライドをループさせるかどうか
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-  nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-  dots: false,//下部ドットナビゲーションの表示
+// $('#slider').slick({
+//   autoplay: true,//自動的に動き出すか。初期値はfalse。
+//   infinite: true,//スライドをループさせるかどうか
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+//   nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+//   dots: false,//下部ドットナビゲーションの表示
+// });
+$(function () {
+  $("#slider").slick({
+    arrows: false,
+    autoplay: true,
+    adaptiveHeight: true,
+    centerMode: true,
+    centerPadding: "7%",
+    dots: false,
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 900, // 399px以下のサイズに適用
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
 });
